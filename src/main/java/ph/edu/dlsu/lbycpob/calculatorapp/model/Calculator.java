ed1653a8b4e75b15d225bb7370e565cdb924dd68
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-29 09:21:52 +0800

    testing

--- src/main/java/ph/edu/dlsu/lbycpob/calculatorapp/model/Calculator.java
+++ src/main/java/ph/edu/dlsu/lbycpob/calculatorapp/model/Calculator.java
@@ -36,10 +36,6 @@ public abstract class Calculator {
      */
     public static final double PI = Math.PI;
 
-    /**
-     * The mathematical constant e (Euler's number) ≈ 2.71828
-     */
-    protected static final double E = Math.E;
 
     /**
      * Abstract method that must be implemented by subclasses to define specific operations.
